0c3f67ef6c8933c02a2263d4adb8116081e40b64
Adewumi Sunkanmi <sunkanmiadewumi1@gmail.com>
2023-01-07 12:26:32 +0100

    updated all memory]

--- src/Ownership/ownership.rs
+++ src/Ownership/ownership.rs
@@ -1,7 +1,98 @@
-fn main(){
-    let x = 5;
-    let y = x; 
-    let name = String::from("hello");
-    let name2 = name;
-    println!("{} String here", name);
-}
+fn main() {
+    // let x = 5;
+
+    // movei(x);
+    // println!("name is {}", x);
+     // move does not happen for simple primitive types
+                               // let name = String::from("hello");
+                               // let name2 = name;
+                               // println!("{} String here", name);
+
+    // let name = String::from("hello");
+    // moves(name);
+    // println!("{} String here", name); // a move has happend here an error will be diplayed
+
+    // Not recommended but you can take and give ownership
+    // let mut s1 = String::from("Sunkanmi");
+    // let length = length(&s1);
+    // change(&mut s1);
+    // let s2 = ownership(s1);
+    // println!("{} String here", s2);
+
+    // reference recommended
+
+    /*
+        this will throw an error because you can't have two mutable reference to a single variable to prevent DATA RACE (think of two threads
+        with shared access to read or modify a single resources without control  )
+    */
+    // let mut s = String::from("test");
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+
+    // works(all reference immutable)
+    // let s = String::from("test");
+    // let r1 = &s;
+    // let r2 = &s;
+    // println!("{}, {}", r1, r2);
+
+    // this will fail because RUST does not allow you to have both mutable and immutable reference of thesame variable together(think of you must be specific, don't confuse the compiler :) )
+    // let mut s = String::from("test");
+    // let r1 = &s;
+    // let r2 = &s;
+    // let r3 = &mut s;
+
+    // println!("{}, {}", r1, r2);
+
+    // Note that the scope of a reference begins when its first introduced and ends the last time its used so this code will work
+    // let mut s = String::from("test");
+    // let r1 = &s;
+    // let r2 = &s;
+    // println!("{}, {}", r1, r2);
+    // let r3 = &mut s;
+    // println!("{} ", r3);
+
+    // DANGLING POINTERS
+    // let reference = dangle();
+
+    //  let mut s = String::from("Hello world");
+    //  let length = firstword(&string); 
+    //  s.clear();
+
+      
+} 
+
+// won't work because you are borrowing a value that will be dropped immediatly after return (RUST NOT PERMIT DANGLING POINTERS) 
+// fn dangle() -> &String {
+//     let s = String::from("Scifi");
+//     &s
+// }
+
+// fn firstword(s: &String)->usize{
+//    let bytes =  s.as_bytes();
+//   for (i , &item) in bytes.inter().enumerate(){
+//      if item = b' '{
+//         return i;
+//      }
+//   }
+//   s.len()
+// } 
+
+// fn moves(name: String) {
+//     println!("name is {}", name);
+// }
+// fn movei(x: i32) {
+//     println!("name is {}", x);
+// }
+
+// fn ownership(s1: String) -> String {
+//     s1
+// }
+
+// fn length(s: &String) -> usize {
+//     s.len()
+// }
+
+// fn change(s: &mut String) {
+//     s.push_str("hello");
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,29 @@
-fn main() {
-    let x = 5;
+ fn main(){
 
-    movei(x);
-    println!("name is {}", x); // move does not happen for simple primitive types
-                               // let name = String::from("hello");
-                               // let name2 = name;
-                               // println!("{} String here", name);
+    //  let mut s = String::from("Hello world");
+    //  let length = firstword(&s); 
+    //  s.clear();
+    // this is not good because even after clearing the string,  we can still print its length 
+    // the only option we have is to keep the "length" in sync with "s" which is also not recommended
+    //  println!("First word length is {}", length);
+    // let mut s = String::from("Hello World");
+    // let hello = &s[0..1];
+    // let world = &s[..]; 
+    
+    // let firstname = firstword(&s);
+    // s.clear();
+    // println!("First name {}", firstname);
+     
+} 
 
-    // let name = String::from("hello");
-    // moves(name);
-    // println!("{} String here", name); // a move has happend here an error will be diplayed
-
-    // Not recommended but you can take and give ownership
-    let mut s1 = String::from("Sunkanmi");
-    let length = length(&s1);
-    change(&mut s1);
-    let s2 = ownership(s1);
-    println!("{} String here", s2);
-
-    // reference recommended
-
-    /*
-        this will throw an error because you can't have two mutable reference to a single variable to prevent DATA RACE (think of two threads
-        with shared access to read or modify a single resources without control  )
-    */
-    // let mut s = String::from("test");
-    // let r1 = &mut s;
-    // let r2 = &mut s;
-    // println!("{}, {}", r1, r2);
-
-    // works(all reference immutable)
-    // let s = String::from("test");
-    // let r1 = &s;
-    // let r2 = &s;
-    // println!("{}, {}", r1, r2);
-
-    // this will fail because RUST does not allow you to have both mutable and immutable reference of thesame variable together(think of you must be specific, don't confuse the compiler :) )
-    // let mut s = String::from("test");
-    // let r1 = &s;
-    // let r2 = &s;
-    // let r3 = &mut s;
-
-    // println!("{}, {}", r1, r2);
-
-    // Note that the scope of a reference begins when its first introduced and ends the last time its used so this code will work
-    // let mut s = String::from("test");
-    // let r1 = &s;
-    // let r2 = &s;
-    // println!("{}, {}", r1, r2);
-    // let r3 = &mut s;
-    // println!("{} ", r3);
-
-    // DANGLING POINTERS
-    let reference = dangle();
-}
-
-fn dangle() -> &String {
-    let s = String::from("Scifi");
-    &s
-}
-
-fn moves(name: String) {
-    println!("name is {}", name);
-}
-fn movei(x: i32) {
-    println!("name is {}", x);
-}
-
-fn ownership(s1: String) -> String {
-    s1
-}
-
-fn length(s: &String) -> usize {
-    s.len()
-}
-
-fn change(s: &mut String) {
-    s.push_str("hello");
-}
+ 
+fn firstword(s: &String)->&str{
+   let bytes =  s.as_bytes();
+  for (i , &item) in bytes.iter().enumerate(){
+     if item == b' '{
+        return  &s[..i]
+     }
+  }; 
+  &s[..]
+} 
+ 
