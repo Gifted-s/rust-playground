61864dddb0ab80feb726a5f352300910b3ed00b9
Adewumi Sunkanmi <sunkanmiadewumi1@gmail.com>
2023-01-07 21:53:04 +0100

    learnings today

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
- fn main(){
+fn main() {
     let x = Some(4);
-    let plus_one = plus_one(x) ;
+    let plus_one = plus_one(x);
     println!("Plus one , {:?}", plus_one);
- }
+}
 
-
-
- fn plus_one (x: Option<i32>)-> Option<i32>{
-   match x {
-    Some(val) => Some( val + 1 ),
-    // None=>None,
-    _=>None,
-   }
-
-
- } 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(val) => Some(val + 1),
+        // None=>None,
+        _ => None,
+    }
+}
