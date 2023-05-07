2ba47285efd67ebfb15198b2cde00d6a04a1c454
Adewumi.Sunkanmi <Adewumi.Sunkanmi@acronis.com>
2023-05-07 21:59:11 +0100

    Making sense, fianally

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-fn main() {
-    let x = Some(4);
-    let plus_one = plus_one(x);
-    println!("Plus one , {:?}", plus_one);
-}
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(val) => Some(val + 1),
-        // None=>None,
-        _ => None,
-    }
+fn main(){
+    println!("Lets Get Rusty")
 }
