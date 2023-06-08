fccec80a5620c168a10430bcd49f709b8cf2258f
Adewumi.Sunkanmi <Adewumi.Sunkanmi@acronis.com>
2023-06-08 13:16:32 +0100

    lint

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -14,14 +14,14 @@ fn generate_workout(intensity: u32, random_number: u32) {
         n
     });
     if intensity < 25 {
-        println!("Today, do {} pushups!",cached_value.value(intensity) );
+        println!("Today, do {} pushups!", cached_value.value(intensity));
 
         println!("Next, do {} situps!", cached_value.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remeber to stay hydrated");
         } else {
-            println!("Today, run for {} minutes!",  cached_value.value(intensity));
+            println!("Today, run for {} minutes!", cached_value.value(intensity));
         }
     }
 }
@@ -34,7 +34,7 @@ where
     value: Option<u32>,
 }
 
-impl<T> Cacher<T> 
+impl<T> Cacher<T>
 where
     T: Fn(u32) -> u32,
 {
